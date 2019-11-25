8bffa518e048771de0a76b73167b0031f943b0e8
Jan Weinen <30630373+janweinen@users.noreply.github.com>
2019-11-25 15:22:45 +0100

    removed unnecessary import

--- src/components/helpers/backgroundImage.js
+++ src/components/helpers/backgroundImage.js
@@ -1,6 +1,5 @@
 import Earth from "../../assets/images/Earth.jpg";
 import Nessus from "../../assets/images/Nessus.jpg";
-import { firestoreRequest } from "../Firebase";
 
 export const getBGImagePath = async location => {
   console.log(location);
