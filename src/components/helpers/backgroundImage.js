5c50bb0f76e2e0c1564f1e0a03fd1b2d740ba81b
Jan Weinen <30630373+janweinen@users.noreply.github.com>
2019-11-25 15:24:38 +0100

    removed unnecessary import

--- src/components/helpers/backgroundImage.js
+++ src/components/helpers/backgroundImage.js
@@ -2,7 +2,6 @@ import Earth from "../../assets/images/Earth.jpg";
 import Nessus from "../../assets/images/Nessus.jpg";
 
 export const getBGImagePath = async location => {
-  console.log(location);
   switch (location) {
     case "Nessus":
       return Nessus;
